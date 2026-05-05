14bc7a96dcc3577b1b7c307187e99e200e7d1101
Vidit Sharma <sharmavidit2007@gmail.com>
2026-05-06 00:30:22 +0530

    updated extra.h

--- extra.h
+++ extra.h
@@ -19,4 +19,44 @@ public:
         return a - b;
     }
 };
-// template class to manage score
+
+// -------- GENERIC FUNCTIONS --------
+template <typename T>
+T findMax(T arr[], int n)
+{
+    T mx = arr[0];
+    for(int i = 1; i < n; i++)
+        if(arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+template <typename T>
+T findMin(T arr[], int n)
+{
+    T mn = arr[0];
+    for(int i = 1; i < n; i++)
+        if(arr[i] < mn)
+            mn = arr[i];
+    return mn;
+}
+
+// -------- INPUT VALIDATION --------
+int getValidInteger()
+{
+    int x;
+    while(true)
+    {
+        cin >> x;
+
+        if(cin.fail())
+        {
+            cout << "Invalid input! Try again: ";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        }
+        else return x;
+    }
+}
+
+#endif
